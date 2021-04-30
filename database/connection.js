a4e0e062cccea7eac2036f9b5384cd5c0d740ae6
rrodriguezg1800 <73864209+rrodriguezg1800@users.noreply.github.com>
2021-04-30 09:16:32 -0500

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "alpine",
+    password: "n0m3l0",
     database: "employees",
     multipleStatements: true,
 });
